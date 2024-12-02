451a0b4591ba7c1c4263d9eb99fec123044f2bf9
DitteKKoustrup <dks@umbraco.dk>
2024-12-02 11:43:07 +0100

    add seetings icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,10 +5,13 @@
 <template>
     <nav class="navbar">
         <div class="logo-container">
-            <img class="logo" src="../assets/images/umbraco_logo_white_horizontal.svg" alt="Umbaeco logo - white">
+            <img class="logo" src="../assets/images/umbraco_logo_white_horizontal.svg" alt="Umbraco logo - white">
         </div>
-        <div class="nav-btn">
+        <div class="nav-btns"> <!-- Changed nav-btn to nav-btns for clarity -->
             <button @click="" class="login-btn">Login</button>
+            <div class="settings-container">
+                <img class="settings-icon" src="../assets/images/settings.svg" alt="Settings icon">
+            </div>
         </div>
     </nav>
 </template>
