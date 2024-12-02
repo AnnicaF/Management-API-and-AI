6f36b46a8c161dd8f84bb04b12abb67ee48d7554
DitteKKoustrup <dks@umbraco.dk>
2024-12-02 10:47:48 +0100

    finished NavBar styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,3 +1,14 @@
+<script setup>
+    import '../assets/style/navBar.css';
+</script>
+
 <template>
-    <div>Test</div>
+    <nav class="navbar">
+        <div class="logo-container">
+            <img class="logo" src="../assets/images/umbraco_logo_white_horizontal.svg" alt="Umbaeco logo - white">
+        </div>
+        <div class="nav-btn">
+            <button @click="" class="login-btn">Login</button>
+        </div>
+    </nav>
 </template>
