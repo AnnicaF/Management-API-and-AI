fb4e2c933be4a7d1addd7dc54b6355d564f74343
DitteKKoustrup <dks@umbraco.dk>
2024-12-02 12:16:52 +0100

    add todos

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,8 +7,9 @@
         <div class="logo-container">
             <img class="logo" src="../assets/images/umbraco_logo_white_horizontal.svg" alt="Umbraco logo - white">
         </div>
-        <div class="nav-btns"> <!-- Changed nav-btn to nav-btns for clarity -->
-            <button @click="" class="login-btn">Login</button>
+        <div class="nav-btns"> 
+            <!-- TODO: Add @click="" -->
+            <button class="login-btn">Login</button> 
             <div class="settings-container">
                 <img class="settings-icon" src="../assets/images/settings.svg" alt="Settings icon">
             </div>
