5cc480ac9372cd8cb20f85e1428488835e5fd8b7
DitteKKoustrup <dks@umbraco.dk>
2024-12-04 16:32:18 +0100

    add styling and active funktion to recycle btn

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,5 +1,14 @@
 <script setup>
     import '../assets/style/sideBar.css';
+    import { ref } from 'vue'; // Import 'ref' to create reactive properties
+
+    // Define the reactive property
+    const isActive = ref(false);
+
+    // Define the toggle method
+    function toggleActiveState() {
+  isActive.value = !isActive.value; // Update the reactive value
+}
 </script>
 
 <template>
@@ -9,7 +18,8 @@
             <img class="add-icon" src="../assets/images/plus.svg" alt="Add icon">
         </div>
 
-        <div class="recycle-bin" onclick="toggleActiveState(event)">
+        <div class="recycle-bin" :class="{ active: isActive }"
+        @click="toggleActiveState">
             <div class="bin-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
             </div>
