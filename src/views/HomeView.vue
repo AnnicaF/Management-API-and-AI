f21cb76172976713f21072cbd649bf834e2401f8
DitteKKoustrup <dks@umbraco.dk>
2024-12-06 16:35:33 +0100

    Add footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <script setup>
 import NavBar from '../components/NavBar.vue'
+import Footer from '../components/Footer.vue'
 </script>
+
 <template>
-<nav>
+<div>
     <NavBar/> 
-</nav>
+    <Footer/>
+</div>
 </template>
