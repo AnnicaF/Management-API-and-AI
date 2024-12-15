224b883942198d8fd4fc3a489e907eb0d959d4fb
Annica <abf@umbraco.dk>
2024-12-15 10:31:00 +0100

    split up more components

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="text-area">
+      <h1>Hello</h1>
+      <h3>Use this AI text generator to effortlessly create content for your next blog. 
+        Once generated, your content will be structured and sent directly to your 
+        Umbraco backoffice for seamless integration.
+      </h3>
+    </div>
+  </template>
+  
+  <style scoped>
+  .text-area {
+    margin: 164px auto 40px; 
+    width: 587px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;  
+    justify-content: center; 
+    text-align: center; 
+  }
+  
+  h1 {
+    font-size: 30px;
+    font-weight: 300;
+    margin-bottom: 20px;
+  }
+  
+  h3 {
+    font-size: 18px;
+    font-weight: 200;
+    line-height: 30px;
+  }
+  </style>
+  
