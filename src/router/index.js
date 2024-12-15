afeb49b7d0366f2ac406ca20c01d3a23d1b7cf9c
Annica <abf@umbraco.dk>
2024-12-15 10:31:57 +0100

    made a Ai response conversation setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
+import ResponseView from '@/views/ResponseView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +14,12 @@ const router = createRouter({
     {
       path: '/home',
       name: 'home',
-      component: HomeView, // Hjemme-side rute
+      component: HomeView,
+    },
+    {
+      path: '/response',
+      name: 'response',
+      component: ResponseView, 
     }
   ],
 })
