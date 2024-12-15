1c092c1a3a3d3c7444a54b3913cfecfab4c96482
Annica <abf@umbraco.dk>
2024-12-15 10:30:07 +0100

    added a new view for respons site

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,22 @@
-<template>
-    <nav>
-        <NavBar/> 
-    </nav>
-    <div class="home-view">
-      <OpenAIComponent />
-    </div>
-  </template>
-  
-  <script setup>
+<script setup>
   import NavBar from '../components/NavBar.vue'
   import UserInfoComponent from '../components/UserInfoComponent.vue';
   import OpenAIComponent from '../components/OpenAIComponent.vue';
-  </script>
+  import ReminderText from '../components/ReminderText.vue';
+  import HeaderComponent from '../components/HeaderComponent.vue';
+</script>
+
+<template>
+  <nav>
+      <NavBar/> 
+  </nav>
+  <div class="home-view">
+    <HeaderComponent />
+    <OpenAIComponent />
+    <ReminderText />
+  </div>
+</template>
+
   
   <style scoped>
   .home-view {
