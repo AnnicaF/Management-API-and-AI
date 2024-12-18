69d5f29b4eab73a681a1abb6e454c64e5e40d44f
DitteKKoustrup <dks@umbraco.dk>
2024-12-18 13:05:58 +0100

    removed toast from homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <script setup>
   import { ref } from "vue";
-  import Toast from "../components/Toasts.vue";
   import NavBar from '../components/NavBar.vue'
   import UserInfoComponent from '../components/UserInfoComponent.vue';
   import OpenAIComponent from '../components/OpenAIComponent.vue';
@@ -8,45 +7,12 @@
   import HeaderComponent from '../components/HeaderComponent.vue';
   import Footer from '../components/Footer.vue'
 
-  const showToast = ref(false);
-  const toastMessage = ref("");
-  const toastType = ref("success");
-
-const showSuccess = () => {
-  toastMessage.value = "Your content has been successfully sent to the Umbraco Backoffice";
-  toastType.value = "success";
-  showToast.value = true;
-  hideToast();
-};
-
-const showError = () => {
-  toastMessage.value = "Error: You cannot send an empty message. Please add content before submitting.";
-  toastType.value = "error";
-  showToast.value = true;
-  hideToast();
-};
-
-const hideToast = () => {
-  setTimeout(() => {
-    showToast.value = false;
-  }, 3000); // Auto-hide toast after 3 seconds
-};  
 </script>
 
 <template>
   <nav>
       <NavBar/> 
   </nav>
-  <div>
-    <button @click="showSuccess">Show Success</button>
-    <button @click="showError">Show Error</button>
-
-    <Toast
-      v-if="showToast"
-      :message="toastMessage"
-      :type="toastType"
-    />
-  </div>
   <div class="home-view">
     <HeaderComponent />
     <OpenAIComponent />
