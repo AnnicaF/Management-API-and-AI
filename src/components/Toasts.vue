9b4289900afc2cfc3f50902ad74d1dff02e3c83f
DitteKKoustrup <dks@umbraco.dk>
2024-12-18 12:39:48 +0100

    initial commit

---
+++ src/components/Toasts.vue
@@ -0,0 +1,27 @@
+<script setup>
+import '../assets/style/toasts.css'
+import { ref, defineProps } from 'vue';
+
+const props = defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: "success", // 'success' or 'error'
+  },
+});
+
+const visible = ref(true);
+</script>
+
+<template>
+    <div
+    v-if="visible"
+    :class="['toast-container', type]"
+  >
+    <span>{{ message }}</span>
+    <button @click="visible = false" class="close-btn">×</button>
+  </div>  
+</template>
