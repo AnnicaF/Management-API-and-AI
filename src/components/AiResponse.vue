4161bcac72856fc9c7e9c510caceb7e3d4967dca
DitteKKoustrup <dks@umbraco.dk>
2025-01-17 10:41:04 +0100

    add edit btn

--- src/components/AiResponse.vue
+++ src/components/AiResponse.vue
@@ -3,7 +3,15 @@ import '../assets/style/aiResponse.css'
 import FooterComponent from './Footer.vue';
 import { messages as messageStore } from '../stores/messageStore.js';
 
+
 const messages = messageStore;
+
+let editToggle = ref(false);
+
+const handleEditMessage = () => {
+  editToggle.value = !editToggle.value;
+  console.log('Changed to:', editToggle.value);
+};
 </script>
 
 <template>
